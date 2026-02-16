45c9bbe9e4483a286793aa3e601de657cca98d46
Christopher Dabrowski <19330758+christopher-dabrowski@users.noreply.github.com>
2026-02-16 18:45:03 +0100

    configure dock

---
+++ .chezmoiscripts/darwin/run_onchange_after_dock.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -eufo pipefail
+
+# Show indicator lights for open applications in the Dock
+defaults write com.apple.dock show-process-indicators -bool true
+
+# Don't automatically hide and show the Dock
+defaults write com.apple.dock autohide -bool false
+
+# Don't show recently used applications in the Dock
+defaults write com.Apple.Dock show-recents -bool false
+
+dockutil --no-restart --remove all
+dockutil --no-restart --add "/Applications/Brave Browser.app"
+dockutil --no-restart --add "/System/Applications/Utilities/Activity Monitor.app"
+dockutil --no-restart --add "/Applications/iTerm.app"
+dockutil --no-restart --add "/Applications/Obsidian.app"
+dockutil --no-restart --add "/Applications/Bitwarden.app"
+dockutil --no-restart --add "~/Downloads" --view auto --display stack --sort dateadded
+
+killall Dock
