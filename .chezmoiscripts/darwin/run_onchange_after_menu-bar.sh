7ab36b94bed737f5d16d38db598d710485c25623
Christopher Dabrowski <19330758+christopher-dabrowski@users.noreply.github.com>
2026-02-17 20:59:54 +0100

    add configuration for Bluetooth, Wi-Fi, and Sound in menu bar

--- .chezmoiscripts/darwin/run_onchange_after_menu-bar.sh
+++ .chezmoiscripts/darwin/run_onchange_after_menu-bar.sh
@@ -11,4 +11,14 @@ defaults write com.apple.menuextra.clock "ShowDate" -int 2
 defaults write com.apple.menuextra.clock "ShowDayOfWeek" -bool false
 
 # Menu bar: show battery percentage
+defaults -currentHost write com.apple.controlcenter Battery -int 18
 defaults -currentHost write com.apple.controlcenter.plist BatteryShowPercentage -bool true
+
+# Show Bluetooth in menu bar
+defaults -currentHost write com.apple.controlcenter Bluetooth -int 18
+
+# Wi-Fi
+defaults -currentHost write com.apple.controlcenter WiFi -int 18
+
+# Sound / Volume
+defaults -currentHost write com.apple.controlcenter Sound -int 18
