caa1209fdb3e4cc7795976d1669b75210f27ba4d
Christopher Dabrowski <19330758+christopher-dabrowski@users.noreply.github.com>
2026-03-06 19:16:30 +0100

    configure swipe gestures for aerospace

--- dot_hammerspoon/init.lua
+++ dot_hammerspoon/init.lua
@@ -21,3 +21,27 @@ leftRightHotkey:bind({"l⌥"}, "n", function()
 end)
 
 leftRightHotkey:start()
+
+local current_id = nil
+local swipe_threshold = 0.08
+
+hs.loadSpoon("Swipe")
+spoon.Swipe:start(4, function(direction, distance, id)
+    -- Identify the start of a new gesture
+    if id ~= current_id then
+        current_id = id
+        swipe_threshold = 0.08 -- Reset the swipe threshold for the new gesture
+    end
+
+    if distance > swipe_threshold then
+        -- Setting the threshold to infinity prevents further triggers within the same gesture
+        swipe_threshold = math.huge 
+
+        if direction == "left" then
+            os.execute("/usr/local/bin/aerospace focus left")
+        end
+        if direction == "right" then
+            os.execute("/usr/local/bin/aerospace focus right")
+        end
+    end
+end)
