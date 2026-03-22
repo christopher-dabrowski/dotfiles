34f591510085925727125f52d77d2e0f14951b07
Christopher Dabrowski <19330758+christopher-dabrowski@users.noreply.github.com>
2026-03-22 13:43:03 +0100

    hide HammerSpoon icon

--- dot_hammerspoon/init.lua
+++ dot_hammerspoon/init.lua
@@ -6,6 +6,10 @@
 --   hs.notify.new({title="Hammerspoon", informativeText="Hello World"}):send()
 -- end)
 
+-- Hide the Hammerspoon icon
+hs.menuIcon(false)
+hs.dockIcon(false)
+
 require("aerspace_keybinds")
 
 local current_id = nil
@@ -13,31 +17,31 @@ local swipe_threshold = 0.08
 
 hs.loadSpoon("Swipe")
 spoon.Swipe:start(4, function(direction, distance, id)
-    -- Identify the start of a new gesture
-    if id ~= current_id then
-        current_id = id
-        swipe_threshold = 0.08 -- Reset the swipe threshold for the new gesture
-    end
+	-- Identify the start of a new gesture
+	if id ~= current_id then
+		current_id = id
+		swipe_threshold = 0.08 -- Reset the swipe threshold for the new gesture
+	end
 
-    if distance > swipe_threshold then
-        -- Setting the threshold to infinity prevents further triggers within the same gesture
-        swipe_threshold = math.huge
+	if distance > swipe_threshold then
+		-- Setting the threshold to infinity prevents further triggers within the same gesture
+		swipe_threshold = math.huge
 
-        if direction == "left" then
-            os.execute("/usr/local/bin/aerospace focus left")
-        end
-        if direction == "right" then
-            os.execute("/usr/local/bin/aerospace focus right")
-        end
-    end
+		if direction == "left" then
+			os.execute("/usr/local/bin/aerospace focus left")
+		end
+		if direction == "right" then
+			os.execute("/usr/local/bin/aerospace focus right")
+		end
+	end
 end)
 
 require("wifi_audio")
 
 -- Pause music when screen locks
 screenLockWatcher = hs.caffeinate.watcher.new(function(eventType)
-    if eventType == hs.caffeinate.watcher.screensDidLock then
-        os.execute("/usr/local/bin/nowplaying-cli pause")
-    end
+	if eventType == hs.caffeinate.watcher.screensDidLock then
+		os.execute("/usr/local/bin/nowplaying-cli pause")
+	end
 end)
 screenLockWatcher:start()
