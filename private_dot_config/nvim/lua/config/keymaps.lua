ab6124fbd6cd831ac049f47beb063770d1900ecf
Christopher Dabrowski <19330758+christopher-dabrowski@users.noreply.github.com>
2026-04-14 20:08:12 +0200

    use plain q to quit nvimpager

--- private_dot_config/nvim/lua/config/keymaps.lua
+++ private_dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+if _G.nvimpager then
+  -- Use 'q' to quit nvimpager
+  vim.keymap.set({ "n", "v", "x" }, "q", "<cmd>qa!<cr>", { desc = "Quit nvimpager" })
+end
