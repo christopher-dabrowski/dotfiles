b3ad348474aaaa04d3075ea8d2698eb1f6ea3d93
Christopher Dabrowski <19330758+christopher-dabrowski@users.noreply.github.com>
2026-04-19 17:41:28 +0200

    configure hunk jumps for VS Code

--- private_dot_config/nvim/lua/config/keymaps.lua
+++ private_dot_config/nvim/lua/config/keymaps.lua
@@ -6,3 +6,17 @@ if _G.nvimpager then
   -- Use 'q' to quit nvimpager
   vim.keymap.set({ "n", "v", "x" }, "q", "<cmd>qa!<cr>", { desc = "Quit nvimpager" })
 end
+
+if vim.g.vscode then
+  local vscode = require("vscode")
+
+  -- Jump to the next git hunk
+  vim.keymap.set("n", "]h", function()
+    vscode.action("workbench.action.editor.nextChange")
+  end, { desc = "Next git hunk" })
+
+  -- Jump to the previous git hunk
+  vim.keymap.set("n", "[h", function()
+    vscode.action("workbench.action.editor.previousChange")
+  end, { desc = "Previous git hunk" })
+end
