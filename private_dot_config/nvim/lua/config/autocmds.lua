a718058c2b7808ae300a67cae213aa15ab46e716
Christopher Dabrowski <19330758+christopher-dabrowski@users.noreply.github.com>
2026-04-28 12:30:52 +0200

    don't format Obsidian notes with LaziVim

--- private_dot_config/nvim/lua/config/autocmds.lua
+++ private_dot_config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,12 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Disable autoformat for markdown files in the Obsidian directory
+vim.api.nvim_create_autocmd({ "BufReadPre", "BufNewFile" }, {
+  pattern = { "*/Obsidian/*.md" },
+  callback = function()
+    -- Disable autoformat for the current buffer
+    vim.b.autoformat = false
+  end,
+})
