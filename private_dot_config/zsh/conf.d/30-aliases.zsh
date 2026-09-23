a2a96d874491eafa7c56f36b0a2b4f7e0945c32c
Christopher Dabrowski <19330758+christopher-dabrowski@users.noreply.github.com>
2026-09-23 13:28:54 +0200

    move shell aliases do dedicated file

---
+++ private_dot_config/zsh/conf.d/30-aliases.zsh
@@ -0,0 +1,14 @@
+# vim: set filetype=sh:
+
+# Alias for logging in to PW laboratories server
+alias volt="ssh dabrowk1@volt.iem.pw.edu.pl"
+
+# Update all pip3 packages
+alias pip3-upgrade="pip3 list --outdated --format=freeze | cut -d = -f 1 | xargs -n1 pip3 install -U"
+
+# Activate venv
+alias venv-activate="source ./venv/bin/activate"
+
+chezmoi-cd() {
+    cd "$(chezmoi source-path)" || exit 1
+}
