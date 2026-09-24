dbdb28805c4e900b247e3e3cb57c4d467bd51ebb
Christopher Dabrowski <19330758+christopher-dabrowski@users.noreply.github.com>
2026-09-24 20:53:27 +0200

    configure linting markdown in neovim

---
+++ private_dot_config/nvim/lua/plugins/extend-nvim-lint.lua
@@ -0,0 +1,19 @@
+local XDG_CONFIG_DIR = os.getenv("XDG_CONFIG_HOME") or os.getenv("HOME") .. "/.config"
+
+return {
+  {
+    "mfussenegger/nvim-lint",
+    optional = true,
+    opts = {
+      linters = {
+        ["markdownlint-cli2"] = {
+          args = {
+            "--config",
+            XDG_CONFIG_DIR .. "/markdownlint-cli2/config.toml",
+            "--",
+          },
+        },
+      },
+    },
+  },
+}
